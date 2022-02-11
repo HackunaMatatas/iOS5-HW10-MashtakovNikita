1ced3f629eccdf9cd44812f19d4d54c23f07b6cb
Nikita Mashtakov <n-mashtakov@yandex.ru>
2022-02-11 14:07:34 +0700

    Delete comments.

--- iOS5-HW10-MashtakovNikita/ViewController.swift
+++ iOS5-HW10-MashtakovNikita/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  iOS5-HW10-MashtakovNikita
-//
-//  Created by Никита Маштаков on 11.02.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
